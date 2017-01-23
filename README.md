08ac52e02b1209617a3024680c10be40cd8ddb61
Alexander Matos <alexander0885@gmail.com>
2017-01-23 15:32:05 +0000

    making things works

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-### Url Shortener Microservice for [Free Code Camp](https://www.freecodecamp.com/challenges/url-shortener-microservice)
+## URL Shortener Microservice for [Free Code Camp](https://www.freecodecamp.com/challenges/url-shortener-microservice)
+
+Simple JavaScript app that will receive a URL as a parameter and will return a shortened URL in the JSON response.
+
+### Example Creation:
+
+`https://url-shortener-microservice-alexandermatos.c9users.io/new/https://www.youtube.com`
+
+### Example Output:
+
+`{"original_url":"https://www.youtube.com","short_url":"url-shortener-microservice-alexandermatos.c9users.io/HJJLk9Qwx"}`  
+`{"error":"Error details"}`
+
+### Usage:
+
+`https://url-shortener-microservice-alexandermatos.c9users.io/HJJLk9Qwx`
+
+#### Demo: [url-shortener](https://github.com/matosalexander/url-shortener)
+
+I based my code in this tutorial
+[http://lefkowitz.me/thoughts/2016/05/05/men-stack-building-a-url-shortener-with-mongodb-express-and-node-js/](http://lefkowitz.me/thoughts/2016/05/05/men-stack-building-a-url-shortener-with-mongodb-express-and-node-js/)
--- server.js
+++ server.js
@@ -1,12 +1,85 @@
 var express=require('express');
 var path=require('path');
+var mongodb = require('mongodb');
+var config=require('./config');
+var shortid = require('shortid');
+var validUrl = require('valid-url');
 
 var app=express();
-var indexFile=path.join(__dirname, 'index.html');
 
 //Get home page
+var indexFile=path.join(__dirname, 'index.html');
 app.get('/',function(req,res){
    res.sendFile(indexFile); 
+});
+
+//Get Mongo connection to local database
+var mLab='mongodb://'+ config.db.host + '/' + config.db.name;
+var mongoClient=mongodb.MongoClient;
+
+
+//Get route for the creation of new links
+app.get('/new/:url(*)',function(req,res){
+   mongoClient.connect(mLab,function(err,db){
+       if(err)return console.error(err);
+       console.log("Connected to server");
+       
+       var collection=db.collection('links');
+       var param=req.params.url;
+       
+       var local = req.get('host') + "/";
+       var insertLink=function(db,callback){
+          //Verify if the new url is not in the database
+          collection.findOne({"url":param},{short:1,_id:0},function(err,doc){
+             if(err)return console.error(err);
+             if(doc !=null){
+                res.json({original_url: param, short_url: local + doc.short});
+             }else{
+                //verify if url is valid
+                if(validUrl.isUri(param)){
+                   var code=shortid.generate();
+                   var obj={url:param, short:code}
+                   collection.insert([obj]);
+                   res.json({ original_url: param, short_url: local + code })
+                }else{
+                   res.json({ error: "Wrong url format" });
+                }
+             }
+         });
+      };
+      
+      insertLink(db,function(){
+          db.close(); 
+      });
+   }); 
+});
+
+//Get route for find the short link and if exist redirect it
+app.get('/:code',function(req, res) {
+   mongoClient.connect(mLab,function(err,db){
+      if(err)return console.error(err);
+      console.log("Connected to server");
+      
+      var collection=db.collection('links');
+      var param=req.params.code;
+      
+      var find=function(db,callback){
+         collection.findOne({"short":param},{url:1,_id:0},function(err,doc){
+            if(err)return console.error(err);
+            if(doc !==null){
+               res.redirect(doc.url);
+            }else{
+               res.json({error:"No shortlink found"});
+            }
+         });
+      };
+      
+      find(db,function(){
+         db.close();
+      });
+      
+      
+   }); 
 });
 
 
